08eeb3ed88b379b2ae0d8e1d56a87fe63bfa56aa
Md.Nazmul Alom <34202639+CoderNazmulAlom@users.noreply.github.com>
2019-11-05 20:30:53 +0600

    all fragment are set as bottom navigationbar

--- app/src/main/java/com/nazmul/androidbottomnavigationbar/MainActivity.java
+++ app/src/main/java/com/nazmul/androidbottomnavigationbar/MainActivity.java
@@ -5,40 +5,65 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
+
+    private HomeFragment homeFragment;
+    private MusicFragment musicFragment;
+    private CartFragment cartFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.bottombar);
+
+        homeFragment = new HomeFragment();
+        musicFragment = new MusicFragment();
+        cartFragment = new CartFragment();
+        setOurFragment(homeFragment);
+
         bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
             @Override
             public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()){
                     case R.id.home:
                         bottomNavigationView.setItemBackgroundResource(R.color.home);
+                        setOurFragment(homeFragment);
                         break;
 
                     case R.id.music:
                         bottomNavigationView.setItemBackgroundResource(R.color.music);
+                        setOurFragment(musicFragment);
                         break;
 
                     case R.id.cart:
                         bottomNavigationView.setItemBackgroundResource(R.color.cart);
+                        setOurFragment(cartFragment);
                         break;
 
 
-                       
+
 
                 }
 
             }
         });
+
     }
+
+    public void setOurFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainframe,fragment);
+        fragmentTransaction.commit();
+
+    }
+
 }
